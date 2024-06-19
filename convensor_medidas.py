14e2cfe8b79fdc5439db76e6f558bd252984cfaa
Ana Clara de Lima <00001091016008sp@al.educacao.sp.gov.br>
2024-06-19 18:36:21 -0300

    Conversor de medidas

--- convensor_medidas.py
+++ convensor_medidas.py
@@ -7,3 +7,18 @@ class ConversorMedidas:
     
     def converter_m_para_cm():
         pass
+
+    def converter_cm_para_m(valor_cm):
+        valor_metros = valor_cm / 100
+        return valor_metros
+   
+    def converter_m_para_cm():
+        pass
+
+#testing area
+# vamos testar e converter 100cm para metros
+resultado =ConversorMedidas.converter_cm_para_m(100)
+
+# o resultado esperado é 1 metro
+print('Resultado:', resultado)
+
